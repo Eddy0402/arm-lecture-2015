683b8d75a2f7b960ff870f3af313b420d2d43305
Eddy0402 <yukkcake@gmail.com>
2014-08-06 15:15:16 +0800

    complele lab-2

--- lab-2/mutex.s
+++ lab-2/mutex.s
@@ -8,20 +8,25 @@
 	.global lock_mutex
 	.type lock_mutex, function
 lock_mutex:
-        @ INSERT CODE BELOW
-
-        @ END CODE INSERT
-	bx lr
+    push { r3, r4, lr }
+	mov r4, locked
+	.TRY:
+		LDREX r3, [r0]
+		CMP r3, unlocked
+		STREXEQ r3, r4, [r0]
+		CMPEQ r3, unlocked
+		BNE .TRY
+	pop  { r3, r4, pc }
 
 	.size lock_mutex, .-lock_mutex
 
 	.global unlock_mutex
 	.type unlock_mutex, function
 unlock_mutex:
-	@ INSERT CODE BELOW
-        
-        @ END CODE INSERT
-	bx lr
+	push { r3, lr }
+	mov r3, unlocked
+	STR r3, [r0]
+	pop  { r3, pc }
 	.size unlock_mutex, .-unlock_mutex
 
 	.end
