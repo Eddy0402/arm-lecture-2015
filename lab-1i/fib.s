a54ba97bab077fa391e00b6d407a9db337585ced
ItsEddy <itseddy0402@gmail.com>
2015-03-06 15:52:10 +0800

    lib-1i: reduce push/pop option by only use caller-saved register

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -13,19 +13,16 @@ fibonacci:
     itt le
     movle r0, #1
     bxle lr
-	push {r4, r5, lr}
 
-	mov r4 ,#1
-    mov r5, #1
+	mov r2 ,#1
+    mov r3, #1
 .L4:
-	add r0, r4, r5
-	mov r4, r5
-	mov r5, r0
-	
+	add r0, r2, r3
+	mov r2, r3
+	mov r3, r0
 	subs r1, #1
 	bgt .L4
 
-	pop {r4, r5, pc}
-	
+    bx lr
 	.size fibonacci, .-fibonacci
 	.end
