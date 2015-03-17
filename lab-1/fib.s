d0ce620e3df82ec341712ca9d94510b0b2462b07
ItsEddy <itseddy0402@gmail.com>
2015-03-17 13:20:56 +0800

    lab-1: Fix the wrong comment, remove dead code.

--- lab-1/fib.s
+++ lab-1/fib.s
@@ -15,19 +15,14 @@ fibonacci:
 
 	push { r4, r5, lr }
 	sub r0, r0, #1
-	mov r5, r0
+	mov r5, r0 @ index - 1
 	bl fibonacci
-	@ Save the return value of recursive call to fibonacci with R4 - 1 as parameter
 	mov r4, r0
 
 	sub r0, r5, #1
-	@ Save the return value of recursive call to fibonacci with R5 - 1 as parameter
 	bl fibonacci
-	add r0, r4
-	pop { r4, r5, pc}		@EPILOG
-
-	mov r0, #0			@ R0 = 0
-	pop { r4, r5, pc}		@ EPILOG
+	add r0, r4 @ fib(index-2) + fib(index-1)
+	pop { r4, r5, pc }   @EPILOG
 
 	.size fibonacci, .-fibonacci
 	.end
